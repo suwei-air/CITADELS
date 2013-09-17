2f93126dab45d83d882ba0a1ca89f56f8ddf9b9a
chennan <chennan@ubuntu.(none)>
2013-09-17 14:46:26 +0800

    help me

--- src/public/javascripts/userlogin.js
+++ src/public/javascripts/userlogin.js
@@ -1,7 +1,10 @@
 
+$(document).ready(function(){
+	$("tr").click(function(){
+		$(this).children().css("background-color","red");
+	});
+});
+
 function check(id){
 	id.value="testuser";
-}
-
-function selectroom(){
 }
