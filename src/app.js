3471f8e498731cb38e2e76258f083b8e3709db74
suwei-air <suwei.air@gmail.com>
2013-09-17 22:53:01 +0800

    change static files to adapt express framework.

--- src/app.js
+++ src/app.js
@@ -31,6 +31,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/test', function(req, res){
+  res.render('test');
+});
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
