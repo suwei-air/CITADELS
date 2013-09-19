a2925f7367b20ca47379aaa9268016876c8c452f
suwei-air <suwei.air@gmail.com>
2013-09-20 00:23:43 +0800

    update room-list

--- src/public/javascripts/room-list.js
+++ src/public/javascripts/room-list.js
@@ -1,10 +1,39 @@
 
 $(document).ready(function(){
-	$("tr").click(function(){
-		$(this).children().css("background-color","red");
+	$("input#username").blur(function(){
+		//$(this).val("testuser");
 	});
-});
+	$("#username-form").submit(function(){
+		alert("your name is '" + $("input#username").val() + "'.");
+		return false;
+	});
+	$("#room-table tr").click(function(){
+		//$(this).children().css("background-color","red");
+		alert("click");
+		$(this).css("background-color","red");
+	});
 
-function check(id){
-	id.value="testuser";
-}
+
+	// just for testing, to generate room list from json data(which will come from db through websocket)
+	var room_info = {"rooms": [
+		{"name": "first room", "timeout": 30, "players": 5, "seats": 8},
+		{"name": "second room", "timeout": 15, "players": 3, "seats": 8}
+	]};
+
+	var room_id = 0;
+	for (var i in room_info.rooms){
+		var room = room_info.rooms[i];
+		console.log(room.name);
+		var element = $("#room" + room_id);
+		$(element).children(".td-room-name").html(room.name);
+		$(element).children(".td-timeout").html(room.timeout + " s");
+		$(element).children(".td-players").html(room.players + "/" + room.seats);
+		++room_id;
+		if (room_id > 4){
+			break;
+		}
+	}
+	for (room_id; room_id<5; ++room_id){
+		$("#room" + room_id).children().html("");
+	}
+});
