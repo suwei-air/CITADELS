d688f7266b867ed27711cd715f6cdee030f4078e
chennan <chennan@ubuntu.(none)>
2013-09-25 17:13:36 +0800

    add room-creation

--- src/public/javascripts/room-list.js
+++ src/public/javascripts/room-list.js
@@ -1,17 +1,59 @@
 $(document).ready(function(){
 	var connection = io.connect("http://localhost");
 
+	function shown(e){
+		var isIe=(document.all)?true:false;
+		var objId=document.getElementById(e);
+		var bobjId=document.getElementById("wrap");
+		var bWidth=parseInt(bobjId.scrollWidth);
+	   	var bHeight=parseInt(bobjId.scrollHeight);
+	   	var sWidth=parseInt((bWidth - objId.clientWidth) / 2);
+	   	var sHeight=(bobjId.clientHeight-objId.clientHeight)/2+bobjId.scrollTop;
+
+		var back=document.createElement("div");
+		back.id="back";
+		var styleStr="top:0px;left:0px;position:absolute;z-index:1;background:#666;width:"+bWidth+"px;height:"+bHeight+"px;";
+		styleStr+=(isIe)?"filter:alpha(opacity=70);":"opacity:0.7;";
+		back.style.cssText=styleStr;
+		document.bobjId.appendChild(back);
+    	objId.style.left=sWidth+"px";
+		objId.style.top=sHeight+"px";
+		objId.style.visibility="visible";
+	}
+
+	function shut(e){
+		var num=e;
+		var bobjId=document.getElementById("wrap")
+		document.getElementById(num).style.visibility="hidden";
+		var back=document.getElementById("back");
+		document.bobjId.removeChild(back);
+	}
+
 	$("input#username").blur(function(){
 		//$(this).val("testuser");
 	});
+
 	$("#username-form").submit(function(){
 		connection.emit('commit-username', $("input#username").val());
 		//alert("your name is '" + $("input#username").val() + "'.");
 		return false;
 	});
+
 	$("#room-table tr").click(function(){
 		$("#room-table tr").css("background-color","");
 		$(this).css("background-color","rgb(50,50,50)");
+	});
+
+	$("#create-room").click(function(){
+		shown("create-opt");
+	});
+
+	$("#opt-cancel").click(function(){
+		shut("create-opt");
+	});
+
+	$("#opt-ok").click(function(){
+		//connection.emit('commit-newroom',);
 	});
 
 	connection.on('commit-username', function(data){
