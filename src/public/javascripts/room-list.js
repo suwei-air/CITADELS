f1944daa9a8a5402c0d433b10bce28a37feba056
chennan <chennan@ubuntu.(none)>
2013-09-26 16:41:42 +0800

    delete code

--- src/public/javascripts/room-list.js
+++ src/public/javascripts/room-list.js
@@ -1,34 +1,6 @@
 $(document).ready(function(){
 	var connection = io.connect("http://localhost");
 
-	function shown(e){
-		var isIe=(document.all)?true:false;
-		var objId=document.getElementById(e);
-		var bobjId=document.getElementById("wrap");
-		var bWidth=parseInt(bobjId.scrollWidth);
-	   	var bHeight=parseInt(bobjId.scrollHeight);
-	   	var sWidth=parseInt((bWidth - objId.clientWidth) / 2);
-	   	var sHeight=(bobjId.clientHeight-objId.clientHeight)/2+bobjId.scrollTop;
-
-		var back=document.createElement("div");
-		back.id="back";
-		var styleStr="top:0px;left:0px;position:absolute;z-index:1;background:#666;width:"+bWidth+"px;height:"+bHeight+"px;";
-		styleStr+=(isIe)?"filter:alpha(opacity=70);":"opacity:0.7;";
-		back.style.cssText=styleStr;
-		bobjId.appendChild(back);
-    	objId.style.left=sWidth+"px";
-		objId.style.top=sHeight+"px";
-		objId.style.visibility="visible";
-	}
-
-	function shut(e){
-		var num=e;
-		var bobjId=document.getElementById("wrap")
-		document.getElementById(num).style.visibility="hidden";
-		var back=document.getElementById("back");
-		document.bobjId.removeChild(back);
-	}
-
 	$("input#username").blur(function(){
 		//$(this).val("testuser");
 	});
