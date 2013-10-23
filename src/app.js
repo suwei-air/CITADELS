97ba8dbafd9a4afd4af3340571a8522502f17ab3
suwei-air <suwei.air@gmail.com>
2013-10-23 23:51:26 +0800

    organize the routes and sio files and functions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var sio = require('socket.io');
+var routes = require('./routes');
+var user = require('./routes/user');
+var sioConnection = require('./sio/connection').connection;
 
 var app = express();
 
@@ -32,12 +28,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/room-list', function(req, res){
-  res.render('room-list');
-});
-app.get('/room', function(req, res){
-  res.render('room');
-});
+app.get('/room-list', routes.roomlist);
+app.get('/room', routes.room);
 app.get('/users', user.list);
 
 var server = http.createServer(app);
@@ -46,78 +38,9 @@ server.listen(app.get('port'), function(){
 });
 
 var io = sio.listen(server);
-var users = [];
-var rooms = [
+global.users = [];
+global.rooms = [
   {"name": "first room", "timeout": 30, "maxbuilding":8, "players": 5, "seats": 8},
   {"name": "second room", "timeout": 15, "maxbuilding":9, "players": 3, "seats": 8}
 ];
-function isUsernameValid(username){
-  var i = 0;
-  for (i = 0; i<users.length; ++i){
-    if (username == users[i].username){
-      break;
-    }
-  }
-  if (i != users.length){
-    return false;
-  }
-  else{
-    return true;
-  }
-}
-function putUsername(socket, username){
-  var i = 0;
-  for (i = 0; i<users.length; ++i){
-    if (socket === users[i].connection){
-      break;
-    }
-  }
-  users[i] = {"username": username, "connection": socket};
-}
-io.on('connection', function(socket){
-  var cid = socket.id;
-  console.log('on connection: cid=' + cid);
-  socket.emit('room-list', rooms);
-
-  // Generate username
-  var usernameNum = 0;
-  for (usernameNum; !isUsernameValid("玩家" + usernameNum); ++usernameNum){}
-  var usernameGenerated = "玩家" + usernameNum;
-  socket.emit("username", usernameGenerated);
-  putUsername(socket, usernameGenerated);
-
-  socket.on('commit-username', function(username){
-    console.log(users);
-    console.log("cid=" + cid + ", request username=" + username);
-    if (!isUsernameValid(username)){
-      socket.emit('commit-username', {
-        "result": false,
-        "message": "Username '" + username + "' already in use!"
-      });
-    }
-    else{
-      putUsername(socket, username);
-      socket.emit('commit-username', {
-        "result": true,
-        "message": "Username '" + username + "' confirmed."
-      });
-    }
-  });
-
-  socket.on('commit-newroom',function(newroom){
-    rooms.push(newroom);
-    socket.emit('room-list',rooms);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('on disconnect: cid=' + cid);
-    var i = 0;
-    for (i = 0; i<users.length; ++i){
-      if (socket === users[i].connection){
-        break;
-      }
-    }
-    users.splice(i, 1);
-    console.log(users);
-  });
-});
+io.on('connection', sioConnection);
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index');//, { title: '富饶之城' });
 };
+
+exports.roomlist = function(req, res){
+  res.render('room-list');
+};
+
+exports.room = function(req, res){
+  res.render('room');
+};
