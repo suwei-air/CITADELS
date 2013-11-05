2759ecb590ffe9b42c705bc931da6134bee9006b
suwei-air <suwei.air@gmail.com>
2013-11-06 00:05:34 +0800

    working on game...

--- sio/game.js
+++ sio/game.js
@@ -1,2 +1,49 @@
+var User = require('../utils/user');
+var Room = require('../utils/room');
+var Game = require('../utils/game');
+var SolidData = require('../utils/soliddata');
+
 exports.game = function(socket){
+  var cid = socket.id;
+  var session = socket.handshake.session;
+  console.log('welcome ' + session.name + '[cid=' + cid + ', sid=' + socket.handshake.cookies['sid']
+    + '] to game[' + session.gameid + '].');
+
+  // update user-socket list
+  User.putUsername(session.name, socket);
+
+  // try to join(or watch)
+  Game.join(session.name, session.gameid);
+
+  // boradcast game-info
+  User.foreachUserConn(Game.getUsernameListByGameid(session.gameid), function(s){
+    s.emit('message', { 'sender': 'System', 'message': session.name + ' joined/watching this game.' });
+    s.emit('game-info', Game.getGameStatusById(session.gameid));
+  });
+
+  if ((!Game.isStarted(session.gameid)) && Game.isAllOnSpot(session.gameid)){
+    Game.start(session.gameid);
+    User.foreachUserConn(Game.getUsernameListByGameid(session.game), function(s){
+      s.emit('game-info', Game.getGameStatusById(session.gameid));
+    });
+  }
+
+  socket.on('take-action', function(action){
+    console.log('try to change seat.');
+    var actionResult = Game.takeAction(session.name, session.gameid, action);
+    if (actionResult === false){
+      socket.emit('take-action', { 'result': false, 'message': 'You cannot take this action!' });
+    }
+    else{
+      User.foreachUserConn(Game.getUsernameListByGameid(session.gameid), function(s){
+        s.emit('take-action', actionResult);
+        s.emit('game-info', Game.getGameStatusById(session.gameid));
+      });
+    }
+  });
+
+  socket.on('disconnect', function(){
+    User.remove(socket);
+    console.log('goodbye ' + session.name + '[cid=' + cid + ', sid=' + socket.handshake.cookies['sid'] + ']');
+  });
 };
--- utils/game.js
+++ utils/game.js
@@ -1,5 +1,5 @@
 var games = [];
-var gameSeq = 0; // incresing sequence number of room id
+var gameSeq = 1; // incresing sequence number of room id
 
 exports.init = function(){
   games = [
@@ -18,6 +18,7 @@ exports.init = function(){
           'coins': 4
         }
       ],
+      'onlookers': [],
       'roleInAction': 'Thief',
       'period': 'MoneyOrCard',
       'rolesHidden': [],
@@ -30,5 +31,119 @@ exports.init = function(){
 };
 
 exports.create = function(room){
-  return 0;
+  var game = new Object();
+  game.id = gameSeq++;
+  game.name = room.name;
+  game.timeout = room.timeout;
+  game.maxbuilding = room.maxbuilding;
+  game.players = new Array();
+  var player, i;
+  for (i in room.players){
+    if (room.players[i] === null){
+      continue;
+    }
+    player = new Object();
+    player.name = room.players[i];
+    player.role = null;
+    player.isKilled = false;
+    player.cards = new Array();
+    player.buildings = new Array();
+    player.coins = 0;
+    game.players.push(player);
+  }
+  game.onlookers = new Array();
+  game.roleInAction = null;
+  game.period = null;
+  game.rolesHidden = new Array();
+  game.rolesShow = new Array();
+  game.rolesToChoose = new Array();
+  game.cardStack = new Array();
+  game.cardDiscarded = new Array();
+  games.push(game);
+  return game.id;
 };
+
+function shuffle(arr){
+  var ret = [];
+  var pos;
+  while (arr.length > 0){
+    pos = round(random()*arr.length);
+    ret.push(arr[pos]);
+    arr.splice(pos, 1);
+  }
+  return ret;
+}
+exports.shuffle = shuffle;
+
+function getGameById(gameid){
+  var i = 0;
+  for (i = 0; i<games.length; ++i){
+    if (gameid == games[i].id){
+      break;
+    }
+  }
+  if (i != games.length){
+    return games[i];
+  }
+  else{
+    return false;
+  }
+}
+exports.getGameById = getGameById;
+
+exports.getUsernameListByGameid = function(gameid){
+  var usernames=[];
+  var game = getGameById(gameid);
+  if (game !== false){
+    for (i in game.players){
+      console.log('pushing ' + game.players[i].name);
+      usernames.push(game.players[i].name);
+    }
+    for (i in game.onlookers){
+      console.log('pushing ' + game.players[i].name);
+      usernames.push(game.onlookers[i]);
+    }
+  }
+  console.log('getUsernameListByGameid returns ' + usernames);
+  return usernames;
+};
+
+exports.join = function(username, gameid){
+  var game = getGameById(gameid);
+  if (game !== false){
+    for (i in game.players){
+      if (game.players[i].name == username){
+        console.log(username + ' is a player in Game #' + gameid);
+        return true;
+      }
+    }
+    console.log(username + ' is a onlooker in Game #' + gameid);
+    return true;
+  }
+  return false;
+};
+
+function start(gameid){
+
+}
+exprots.start = start;
+
+function isStarted(gameid){
+
+}
+exports.isStarted = isStarted;
+
+function isAllOnSpot(gameid){
+
+}
+exports.isAllOnSpot = isAllOnSpot;
+
+function getGameStatusById(gameid){
+
+}
+exports.getGameStatusById = getGameStatusById;
+
+function takeAction(username, gameid, action){
+
+}
+exports.takeAction = takeAction;
